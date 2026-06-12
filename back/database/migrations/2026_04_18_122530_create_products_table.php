365842273017a63cdad50127a2c993796bbd8689
Joroh <jorohahmad@gmail.com>
2026-06-12 21:00:35 +0300

    add is active for products

--- back/database/migrations/2026_04_18_122530_create_products_table.php
+++ back/database/migrations/2026_04_18_122530_create_products_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('owner_id')->constrained('users')->cascadeOnDelete();
+            $table->boolean('is_active')->default(false);
             $table->string('title');
             $table->text('description');
             $table->string('image1')->nullable();
